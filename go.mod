57d70056c3b8a8d4af6bffb94ed0836ed66079c7
Roshan Lamichhane <lamichhanersn@gmail.com>
2025-06-23 20:39:08 +0545

    feat(download): added download method
    added methods for downloading and saving a file from books site in the 'downloader' package

--- go.mod
+++ go.mod
@@ -5,6 +5,10 @@ go 1.24.4
 require (
 	github.com/PuerkitoBio/goquery v1.10.3 // indirect
 	github.com/andybalholm/cascadia v1.3.3 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/wneessen/go-mail v0.6.2 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
 	golang.org/x/net v0.39.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
 	resty.dev/v3 v3.0.0-beta.3 // indirect
 )
--- internal/downloader/download.go
+++ internal/downloader/download.go
@@ -1,18 +1,24 @@
 package downloader
 
 import (
-	"bytes"
 	"errors"
 	"fmt"
+	"io"
+	"log/slog"
+	"mime"
 	"net/http"
 	"net/url"
+	"os"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/google/uuid"
 	"resty.dev/v3"
 )
 
 const adsTerm = "ads.php?"
+
+var downloadDir = "./downloads"
 
 var (
 	NilRestyClientErr     = errors.New("no resty clienty provided, expected a resty client reference")
@@ -20,17 +26,19 @@ var (
 	NoLinkFoundAdsPageErr = errors.New("could not extract download link from ads page")
 )
 
+// Process takes the url and attempts to download the file
 func Process(client *resty.Client, url string) error {
-	if client == nil {
-		return NilRestyClientErr
-	}
 	// 1. Check if the url is libgen ads site from which download link is to be extracted or it is download url itself
 	// 2. if libgen ads site, extract the download link
 	// 3. Proceed to download from the url
 	// 4. Save the file to a directory
+	if client == nil {
+		return NilRestyClientErr
+	}
 
 	downloadLink := url
 	if isAdsPage(url) {
+		slog.Info("Extracting download link from ads page: " + url)
 		resp, err := client.R().Get(url)
 		if err != nil {
 			return err
@@ -45,9 +53,56 @@ func Process(client *resty.Client, url string) error {
 			return err
 		}
 	}
-	_ = downloadLink
-	return nil
 
+	return downloadAndSave(client, downloadLink)
+}
+
+// downloadAndSave downloads a file form the provided link and saves it under
+// the downloads(read from env var during start) directory
+func downloadAndSave(client *resty.Client, url string) error {
+	resp, err := client.R().Get(url)
+	if err != nil {
+		return err
+	}
+	slog.Info("Downloaded file from url", slog.String("url", url))
+
+	contentDisposition := resp.Header().Get("Content-Disposition")
+	var filename string
+
+	if contentDisposition != "" {
+		_, params, err := mime.ParseMediaType(contentDisposition)
+		if err == nil && params["filename"] != "" {
+			filename = params["filename"]
+		}
+	}
+
+	// Fallback to use task ID
+	if filename == "" {
+		id := uuid.New()
+		filename = id.String() // TODO: use id as filename as fallback
+		t, _, _ := mime.ParseMediaType(resp.Header().Get("Content-Type"))
+		if t != "" {
+			exts, _ := mime.ExtensionsByType(t)
+			if len(exts) > 0 {
+				filename += exts[0]
+			}
+		}
+	}
+
+	filePath := downloadDir + "/" + filename
+	out, err := os.Create(filePath)
+
+	if err != nil {
+		return fmt.Errorf("error while creating file, %w", err)
+	}
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return fmt.Errorf("error while saving response, %w", err)
+	}
+
+	slog.Info("Saved file", slog.String("filepath", filePath))
+	return nil
 }
 
 // isAdsPage checks if the given urls if an ads page (not advertisement, more like file description)
@@ -79,10 +134,4 @@ func extractDownloadLink(resp *resty.Response) (string, error) {
 
 	link := fmt.Sprintf("%s://%s/%s", parsedURL.Scheme, parsedURL.Host, val)
 	return link, nil
-}
-
-// SaveToDirectory saves the file to the given path
-func SaveToDirectory(path string, content bytes.Buffer) error {
-
-	return nil
 }
