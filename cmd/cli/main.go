81d54a3e65ab4f155994839427d525986c220181
Roshan Lamichhane <lamichhanersn@gmail.com>
2025-07-07 14:46:55 +0545

    feat(cli): initial cli support to download and email the file to receipeint
    A simple cli that supports url intake and downloads it, and emails it to receiver.
    The configuration detail is stored at ~/.config/send-to-kindle/config.yaml

---
+++ cmd/cli/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/roshanlc/send-to-kindle/internal/helper"
+	"gopkg.in/yaml.v3"
+)
+
+const configPath = ".config/send-to-kindle/config.yaml" // from the home directory
+
+// Config holds details about program configuration
+type Config struct {
+	Host         string `yaml:"HOST"`
+	Port         int    `yaml:"PORT"`
+	From         string `yaml:"FROM"`
+	To           string `yaml:"TO"`
+	User         string `yaml:"USERID"`
+	Password     string `yaml:"PASSWORD"`
+	DownloadsDir string `yaml:"DOWNLOADSDIR"`
+}
+
+func main() {
+	// setup logger
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
+	config, err := readConfig()
+	if err != nil {
+		slog.Error(err.Error())
+		return //exit while running deferred functions
+	}
+
+	if len(os.Args) == 1 {
+		slog.Error("no url provided, please provide a single url")
+		fmt.Println("usage: ./send-to-kindle <url>")
+		return
+	}
+
+	// extract  url from args
+	url := extractURL(os.Args[1])
+	if url == "" {
+		slog.Error("please provide a valid url")
+		return
+	}
+
+	slog.Info("extracted url", slog.String("url", url))
+
+	// TODO: add this to database later
+	// process the url
+	process(config, url)
+}
+
+// extractURL takes value from arguments
+func extractURL(rawURL string) string {
+	if helper.IsURLValid(rawURL) {
+		return rawURL
+	}
+	return ""
+}
+
+// readConfig attempts to read config file
+func readConfig() (*Config, error) {
+	var config Config
+
+	userDir, err := os.UserHomeDir()
+	if err != nil {
+		return nil, fmt.Errorf("unable to fetch user directory value: %w", err)
+
+	}
+	file, err := os.ReadFile(userDir + "/" + configPath)
+	if err != nil {
+		return nil, fmt.Errorf("unable to read config file: %w", err)
+	}
+
+	err = yaml.Unmarshal(file, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: also check the validity of Config object
+	// TODO: maybe move config related stuff to internal package
+	return &config, nil
+}
---
+++ cmd/cli/process.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log/slog"
+	"time"
+
+	"github.com/roshanlc/send-to-kindle/internal/downloader"
+	"github.com/roshanlc/send-to-kindle/internal/email"
+	"github.com/roshanlc/send-to-kindle/internal/helper"
+	"resty.dev/v3"
+)
+
+// process takes the url, downloads the file and emails it as an attachment
+func process(config *Config, url string) {
+	ctx := helper.GenerateIDWithContext()
+	slog.Info("trying to download file", slog.String("url", url), slog.Any("taskID", helper.GetIDFromContext(ctx)))
+
+	client := resty.New().
+		SetRetryCount(3).
+		SetTimeout(5 * time.Minute)
+
+	ctx, err := downloader.Process(ctx, client, url)
+	if err != nil {
+		slog.Error("process failed", slog.String("error", err.Error()))
+		return
+	}
+
+	slog.Info("attempting to email downloaded file", slog.Any("taskID", helper.GetIDFromContext(ctx)))
+
+	details := email.EmailDetails{
+		From:        config.From,
+		To:          config.To,
+		Host:        config.Host,
+		Port:        config.Port,
+		Subject:     helper.GetIDFromContext(ctx).String(),
+		Body:        "Save the attached file(s).",
+		Attachments: []string{helper.GetFilepathFromContext(ctx)},
+		Username:    config.User,
+		Password:    config.Password,
+	}
+
+	err = email.Send(ctx, details)
+	if err != nil {
+		slog.Error("process failed while sending email", slog.String("error", err.Error()))
+		return
+	}
+	err = client.Close()
+	if err != nil {
+		slog.Error("process failed during http client closure", slog.String("error", err.Error()))
+		return
+	}
+}
